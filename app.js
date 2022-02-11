290e9271ddd7281237c1fd03523bf1c0d357146f
Jonathan Lindqvist <ocmino@gmail.com>
2022-02-11 12:54:32 +0100

    add game code

--- app.js
+++ app.js
@@ -15,3 +15,50 @@ app.get('/', function(req, res){
 app.listen(PORT, function(){
     console.log('Server started on port ${PORT}')
 })
+
+
+
+
+
+
+var clicks = 0;
+
+function onClick() {
+  clicks += 1;
+  document.getElementById("onclick").innerHTML = clicks;
+};
+
+
+
+var charachter = 
+document.getElementById("charachter");
+var block =
+document.getElementById("block");
+function jump(){
+    if(charachter.classList != "animate"){
+        charachter.classList.add("animate");
+    }
+    charachter.classList.add("animate");
+    setTimeout(function(){
+        charachter.classList.remove("animate");
+    }, 500);
+}
+
+
+var checkDead = setInterval(function(){
+
+    var charachterTop = parseInt(window.getComputedStyle(charachter).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft < 20 && blockLeft > 0 && charachterTop > 130){
+        if(confirm('Du fick ' + clicks + ' poäng. Tryck på  "Ok" för att spela igen.')){
+            window.location = '/'
+        }
+        return 
+    }
+ 
+
+}, 10);
+
+
+
